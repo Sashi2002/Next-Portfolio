56094dea16c384c0653761cbbd37b9c3d9efa4ef
sashikanta mohanty <sm8168@srmist.edu.in>
2023-12-14 22:37:08 +0530

    header & socials

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,33 @@
+import Image from "next/legacy/image";
+import Link from "next/link";
+
+//components
+import Socials from "../components/Socials";
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-8 xl:h-[98px]">
+      <div className="container mx-auto ">
+        <div className="flex flex-col lg:flex-row justify-between items-center py-4">
+          {/* logo */}
+          <Link href={"/"}>
+            <div className="w-[320px] h-[100px] max-sm:w-[300px] relative md:right-28 lg:right-44 brightness-200">
+              <Image
+                src={"/logo.png"}
+                width={220}
+                height={148}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+                priority={true}
+              />
+            </div>
+          </Link>
+          {/* socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,60 @@
+import Link from "next/link";
+import {
+  RiGithubLine,
+  RiLinkedinBoxLine,
+  RiTwitterXLine,
+  RiInstagramLine,
+  RiFacebookBoxLine,
+  RiThreadsLine,
+} from "react-icons/ri";
+
+const socialData = [
+  {
+    platform: "GitHub",
+    icon: <RiGithubLine />,
+    link: "https://github.com/sashi2002",
+  },
+  {
+    platform: "LinkedIn",
+    icon: <RiLinkedinBoxLine />,
+    link: "https://linkedin.com/in/your-username",
+  },
+  {
+    platform: "Twitter",
+    icon: <RiTwitterXLine />,
+    link: "https://twitter.com/your-username",
+  },
+  {
+    platform: "Instagram",
+    icon: <RiInstagramLine />,
+    link: "https://instagram.com/your-username",
+  },
+  {
+    platform: "Thread",
+    icon: <RiThreadsLine />,
+    link: "",
+  },
+  {
+    platform: "Facebook",
+    icon: <RiFacebookBoxLine />,
+    link: "https://facebook.com/your-username",
+  },
+];
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg ">
+      {socialData.map((social, index) => (
+        <Link
+          key={index}
+          href={social.link}
+          className="hover:text-accent transition-all duration-300"
+        >
+          {social.icon}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
