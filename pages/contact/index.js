64ef5659bbdb115ff4d614b452acfb94b8ce7ea1
sashikanta mohanty <sm8168@srmist.edu.in>
2023-12-20 22:51:08 +0530

    update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -84,7 +84,6 @@ const Contact = () => {
             animate="show"
             exit="hidden"
             className="flex-1 flex flex-col gap-6 w-full mx-auto"
-            // onSubmit={onSubmit}
           >
             {/* input group */}
             <div className="flex gap-x-6 w-full">
